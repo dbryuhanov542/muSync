9de2fdccfeb7abd8f3a577c22e919afb414dcc8f
lvstrk <d.bryuhanov542@yandex.ru>
2023-02-10 09:38:57 +0300

    Space calc

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 from ftplib import *
 import sys
+import math
 
 ftp = FTP()
 
@@ -11,7 +12,7 @@ ftp = FTP()
 # 
 
 dest_dir = 'D:\hello'
-remote_dir = 'Music'
+remote_dir = 'test 123'
 
 # 
 # Functions
@@ -40,12 +41,44 @@ def _is_ftp_dir(ftp_data, remote_path): # Determines if a directory is a directo
         return False
 
 
+def _check_if_contains_local(filename, dest_dir):
+    print('Checking for: {0}'.format(filename))
+    if filename in os.listdir():
+        print('Already downloaded: {0}\nSkipping...'.format(filename))
+        return True
+    else:
+        return False
+
+
 def _mirror_ftp_dir(remote_path, dest_curdir):  # Mirrors directory from ftp to local machine
     mirrored_dir = os.path.join(dest_curdir, remote_path)
     print('Trying to make mirror: ' + mirrored_dir)
     if not os.path.exists(mirrored_dir):
         os.makedirs(mirrored_dir)
         print('created dir: {0}'.format(mirrored_dir))
+
+
+# def calculate_usage_of_space(ftp_data, remote_path, dest_path, overall_size):
+#     original_ftp_dir = ftp_data.pwd()
+#     for name in ftp_data.nlst():
+#         if _is_ftp_dir(ftp, name):
+#             ftp.cwd(name)
+#             original_os_dir = os.getcwd()
+#             calculate_usage_of_space(ftp, name, dest_dir + '\\' + name, overall_size)
+#             ftp.cwd(original_ftp_dir)
+#         else:
+#             if _check_if_contains_local(name, dest_path) == False:
+#                 overall_size += ftp.size(name)
+#             print(overall_size)
+#             ftp_data.cwd(original_ftp_dir)
+#             pass
+#             return overall_size
+
+
+def human_read_format(size):
+    pwr = math.floor(math.log(size, 1024))
+    suff = ['B', 'KB', 'MB', 'GB']
+    return f"{size / 1024 ** pwr:.0f}{suff[pwr]}"
 
 
 def _download_ftp_file(ftp_data, filename, dest_path):  # Downloads single file
@@ -72,18 +105,22 @@ def download_ftp_tree(ftp_data, remote_path, dest_path):    # Checking for direc
             os.chdir(original_os_dir)
             ftp.cwd(original_ftp_dir)
         else:
-            _download_ftp_file(ftp_data, name, dest_path)
+            if _check_if_contains_local(name, dest_path) == False:
+                _download_ftp_file(ftp_data, name, dest_path)
             ftp_data.cwd(original_ftp_dir)
-            print('back to: ' + original_ftp_dir)
+            print('Back to: ' + original_ftp_dir)
             pass
+
 
 # 
 # Main executions
+# 
+
 if __name__ == '__main__':
     _open_conn(ftp)
     print('Connection opened')
     os.chdir(dest_dir)
     ftp.cwd(remote_dir)
+    # print(calculate_usage_of_space(ftp, remote_dir, dest_dir))
     download_ftp_tree(ftp, remote_dir, dest_dir)
-    print(format(ftp.nlst()))
-    
+    print("Downloading success")
